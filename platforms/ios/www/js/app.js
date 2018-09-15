3b4205e3b056a8e3135e1883ea9b57d142a8a3e0
Erickson Teixeira <erickson.teixeira@linkka.com.br>
2018-09-15 01:56:22 -0300

    Commit Garcon App Cordova

---
+++ platforms/ios/www/js/app.js
@@ -0,0 +1,38 @@
+$('.collection')
+    .on('click', '.collection-item', function(){
+
+        var $badge = $('.badge', this);
+        if ($badge.length === 0) {
+            $badge = $('<span class="badge brown-text">0</span>')
+                        .appendTo(this);
+        }
+
+        $badge.text(parseInt($badge.text()) + 1);
+    })
+
+$('.modal-trigger').leanModal();
+
+$('#confirmar').on('click', function() {
+
+    var texto = ' ';
+
+	$('.badge').parent().each(function() {
+	    var produto = this.firstChild.textContent;
+	    var quantidade = this.lastChild.textContent;
+
+	    texto += produto + ': ' + quantidade + ', ';
+	});
+	
+    $('#resumo').empty().text(texto);
+});
+ 
+
+$('.collection').on('click', '.badge', function(){
+    $(this).remove();
+    return false;
+});
+
+$('.acao-limpar').on('click', function() {
+    $('#numero-mesa').val( '' );
+    $('.badge').remove();
+});
